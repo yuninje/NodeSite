c1e59a54b8922ff385d839ea1158689015b2579d
yuninje <dbsdlswp@naver.com>
2020-01-14 10:55:28 +0900

    .

--- models/index.js
+++ models/index.js
@@ -16,12 +16,28 @@ db.Comment = require('./comment')(sequelize,Sequelize);
 
 db.User.hasMany(db.Post, {onDelete: 'cascade'});
 db.User.hasMany(db.Comment, {onDelete: 'cascade'});
+db.User.hasMany(db.Post, { through : 'Like'} );
+
+db.User.belongsToMany(db.User, {
+  foriegnKey : followgingId,
+  as : Followers,
+  through : 'Follow',
+});
+db.User.belongsToMany(db.User, {
+  foriegnKey : followwerId,
+  as : Followings,
+  through : 'Follow',
+})
+
 
 db.Post.belongsTo(db.User);
+db.Post.belongsTo(db.User, { through : 'Like'});
 db.Post.hasMany(db.Comment, {onDelete: 'cascade'});
 
 db.Comment.belongsTo(db.User);
 db.Comment.belongsTo(db.Post);
+
+
 
 
 module.exports = db;
